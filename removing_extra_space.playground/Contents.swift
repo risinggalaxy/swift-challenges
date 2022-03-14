6a827c57105c228b49bf6497eff54cc4767dec0e
Yasser Farahi <yfarahi@risinggalaxy.com>
2022-03-14 15:17:25 +0100

    Added unittests to removewhitespaces

--- removing_extra_space.playground/Contents.swift
+++ removing_extra_space.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 /*
  3. Removing Extra Space
  Given a proposed String, write an in-place algorithm to remove all whitespace. For this challenge,
@@ -7,39 +8,70 @@ import Foundation
  How will your function search through the the data?
  How will you track the removal of data? Will your solution make use of fast enumeration? Why or why not?
  */
-
-func removeWhiteSpace(_ input: String) -> String {
-    guard input.count > 1 else { return input }
-    let chars = Array(input)
-    var str = ""
-    let m = chars.count
-    var n = 0
-    while n < m {
-        if chars[n] != " " {
-            str.append(chars[n])
+class RemoveExtraWhiteSpace {
+    func removeWhiteSpace(_ input: String) -> String {
+        guard input.count > 1 else { return input }
+        let chars = Array(input)
+        var str = ""
+        let m = chars.count
+        var n = 0
+        while n < m {
+            if chars[n] != " " {
+                str.append(chars[n])
+            }
+            n += 1
         }
-        n += 1
+        return str
     }
-    return str
+    
+    func removeWhiteSpace(_ input: inout String)  {
+        guard input.count > 1 else { return }
+        let chars = Array(input)
+        var str = ""
+        let m = chars.count
+        var n = 0
+        while n < m {
+            if chars[n] != " " {
+                str.append(chars[n])
+            }
+            n += 1
+        }
+        input = str
+    }
 }
 
-func removeWhiteSpace(_ input: inout String)  {
-    guard input.count > 1 else { return }
-    let chars = Array(input)
-    var str = ""
-    let m = chars.count
-    var n = 0
-    while n < m {
-        if chars[n] != " " {
-            str.append(chars[n])
-        }
-        n += 1
+class TestRemoveExtraWhiteSpace: XCTestCase {
+    func testRemoveWhiteSpaceHappyPath() {
+        let removeExtraWS = RemoveExtraWhiteSpace()
+        let input = "The qu ick fox jum ped over the lazy d o g"
+        let result = removeExtraWS.removeWhiteSpace(input)
+        let testData = "Thequickfoxjumpedoverthelazydog"
+        XCTAssertEqual(result, testData)
     }
-    input = str
+    
+    func testRemoveWhiteSpaceInout() {
+        let removeExtraWS = RemoveExtraWhiteSpace()
+        var input = "The qu ick fox jum ped over the lazy d o g"
+        removeExtraWS.removeWhiteSpace(&input)
+        let testData = "Thequickfoxjumpedoverthelazydog"
+        XCTAssertEqual(input, testData)
+    }
+    
+    func testRemoveWhiteSpace() {
+        let removeExtraWS = RemoveExtraWhiteSpace()
+        let input = " T h e q u   i ck f  o x j  u   m p  e  d  ov   e   r t  h  e l a  zy d o g     "
+        let result = removeExtraWS.removeWhiteSpace(input)
+        let testData = "Thequickfoxjumpedoverthelazydog"
+        XCTAssertEqual(result, testData)
+    }
+    
 }
 
-var input = "The qu ick fox jum ped over the lazy d o g"
-let result = removeWhiteSpace("The qu ick fox jum ped over the lazy d o g")
-removeWhiteSpace(&input)
-print(result)
-print(input)
+class TestObservation: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase, didRecord issue: XCTIssue) {
+        assertionFailure(issue.description)
+    }
+}
+let testObservation = TestObservation()
+XCTestObservationCenter.shared.addTestObserver(testObservation)
+XCTestSuite.default.run()
